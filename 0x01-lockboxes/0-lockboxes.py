03a5fcaae77813d18b7a91ff72257312abb5a52e
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-07-09 21:30:32 +0100

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,19 +10,38 @@ def canUnlockAll(boxes):
     """
     Method that determines if all the boxes can be opened
     """
+    num_of_boxes = len(boxes)
+    opened = [False] * num_of_boxes
+    opened[0] = True
+    keys = []
 
-    visited = [False] * len(boxes)
-    visited[0] = True
-    
-    def unlockBox(box):
-        if visited[box]:
+    print(opened)
+
+    def get_keys(box):
+
+        """
+        Adds new keys to key list
+        """
+        for key in box:
+            if key not in keys:
+                keys.append(key)
+
+    def checkBox(box):
+        if opened[box]:
             return
-        
-        visited[box] = True
-        
-        for key in boxes[box]:
-            unlockBox(key)
+        elif len(boxes[box]) and box in keys:
+            opened[box] = True
+
+    get_keys(boxes[0])
+
+    for box in range(num_of_boxes):
+        get_keys(boxes[box])
+        checkBox(box)
+
+    for box in range(num_of_boxes):
+        get_keys(boxes[box])
+        checkBox(box)
+
+    print(opened)
     
-    unlockBox(0)
-    
-    return all(visited)
+    return all(opened)

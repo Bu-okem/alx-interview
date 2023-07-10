3284ff351495cdb10a66e4a304b6f19e0d46fd82
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-07-10 07:00:12 +0100

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,48 +5,11 @@ Documentation
 """
 
 
-# def canUnlockAll(boxes):
-
-#     """
-#     Method that determines if all the boxes can be opened
-#     """
-#     num_of_boxes = len(boxes)
-#     opened = [False] * num_of_boxes
-#     opened[0] = True
-#     keys = []
-
-
-#     def get_keys(box):
-
-#         """
-#         Adds new keys to key list
-#         """
-#         for key in box:
-#             if key not in keys:
-#                 keys.append(key)
-
-#     def checkBox(box):
-
-#         """
-#         Opens box and gets the keys if it can be opened and isn't open
-#         """
-#         if opened[box]:
-#             return
-#         elif box in keys:
-#             opened[box] = True
-#             get_keys(boxes[box])
-
-#     get_keys(boxes[0])
-
-#     for box in range(num_of_boxes):
-#         checkBox(box)
-
-#     for box in range(num_of_boxes):
-#         checkBox(box)
-    
-#     return all(opened)
-
 def canUnlockAll(boxes):
+
+    """
+    Method that determines if all the boxes can be opened
+    """
     num_of_boxes = len(boxes)
     opened = {0}  # Using a set for opened boxes
     keys = set(boxes[0])  # Using a set for keys
@@ -55,6 +18,10 @@ def canUnlockAll(boxes):
         keys.update(boxes[box])
 
     def check_box(box):
+
+        """
+        Opens box and gets the keys if it can be opened and isn't open
+        """
         opened.add(box)
         get_keys_from_box(box)
 
